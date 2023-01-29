80795bc8cba9931bd41ccd737884cbdb8df3fa0f
bananabread08 <rmkfermin@gmail.com>
2023-01-30 07:30:20 +0800

    update StatTable with extracted components

--- src/components/StatTable.tsx
+++ src/components/StatTable.tsx
@@ -1,11 +1,15 @@
 import { CharData } from '@/data';
-
+import { Rows } from './Rows';
+import { useState } from 'react';
 type TableProps = {
   data: CharData[];
   title: string;
 };
 
 export const StatTable = ({ data, title }: TableProps) => {
+  const [sorted, setSorted] = useState([...data]);
+  const [active, setActive] = useState('name');
+  const dataToShow = title === 'Computed Growth' ? data : sorted;
   const tableHeaders = [
     'HP',
     'STR',
@@ -17,6 +21,16 @@ export const StatTable = ({ data, title }: TableProps) => {
     'RES',
     'BLD',
   ];
+  const handleSort = (head: string) => {
+    console.log(head);
+    const index = tableHeaders.findIndex((e) => e === head);
+    const newData = sorted.sort((a, b) =>
+      a.growth[index] > b.growth[index] ? -1 : 1
+    );
+    setSorted([...newData]);
+    setActive(head);
+  };
+
   return (
     <table>
       <caption>{title}</caption>
@@ -24,27 +38,21 @@ export const StatTable = ({ data, title }: TableProps) => {
         <tr>
           <th>Name</th>
           {tableHeaders.map((head) => (
-            <th key={head}>{head}</th>
+            <th
+              onClick={
+                title === 'Computed Growth' ? undefined : () => handleSort(head)
+              }
+              key={head}
+            >
+              <p className={active === head ? 'active' : ''}>{head}</p>
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {data.map((obj) => {
-          const rowClass =
-            obj.name === 'Starsphere'
-              ? 'star'
-              : obj.name === 'Total'
-              ? 'total'
-              : '';
-          return (
-            <tr key={obj.name} className={rowClass}>
-              <td>{obj.name}</td>
-              {obj.growth.map((stat, index) => (
-                <td key={obj.name + tableHeaders[index]}>{stat} </td>
-              ))}
-            </tr>
-          );
-        })}
+        {dataToShow.map((obj) => (
+          <Rows key={obj.name} obj={obj} tableHeaders={tableHeaders} />
+        ))}
       </tbody>
     </table>
   );
