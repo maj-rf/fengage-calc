9e0e8028ff29fd6144cf8c1aa235bd3d71da6702
bananabread08 <rmkfermin@gmail.com>
2023-02-03 09:30:21 +0800

    extract utils functions from pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,21 @@ import { StatTable } from '@/components/StatTable';
 import { characterData, classData } from '@/data';
 import { useState } from 'react';
 import { Selectors } from '@/components/Selectors';
+import {
+  getFinalGrowth,
+  getMaxStats,
+  getSelectedClass,
+  getStarsphere,
+} from '@/utils';
 
 export default function Home() {
   const [currentChar, setCurrentChar] = useState(characterData[1]);
   const [currentClass, setCurrentClass] = useState(classData[0]);
-  const selectedClass =
-    currentChar.name === 'Jean'
-      ? {
-          ...currentClass,
-          growth: currentClass.growth.map((c) => c * 2),
-        }
-      : currentClass;
-  const finalGrowth = currentChar.growth.map(
-    (v, i) => v + selectedClass.growth[i]
-  );
-  const starsphere = finalGrowth.map((v) => v + 15);
-  const maxStats = currentChar.mods.map((v, i) => v + selectedClass.mods[i]);
+
+  const selectedClass = getSelectedClass(currentChar, currentClass);
+  const finalGrowth = getFinalGrowth(currentChar, selectedClass);
+  const starsphere = getStarsphere(finalGrowth);
+  const maxStats = getMaxStats(currentChar, selectedClass);
 
   const handleCharacter = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const char = characterData.find((obj) => obj.name === e.target.value);
---
+++ src/utils.ts
@@ -0,0 +1,26 @@
+import { CharData } from '@/data';
+export function getSelectedClass(
+  currentChar: CharData,
+  currentClass: CharData
+) {
+  return currentChar.name === 'Jean'
+    ? {
+        ...currentClass,
+        growth: currentClass.growth.map((c) => c * 2),
+      }
+    : currentClass;
+}
+
+export function getFinalGrowth(currentChar: CharData, selectedClass: CharData) {
+  return currentChar.growth.map((v, i) => v + selectedClass.growth[i]);
+}
+
+export function getStarsphere(finalGrowth: number[]) {
+  return finalGrowth.map((v, i) => {
+    return i === 9 ? v + 135 : v + 15;
+  });
+}
+
+export function getMaxStats(currentChar: CharData, selectedClass: CharData) {
+  return currentChar.mods.map((v, i) => v + selectedClass.mods[i]);
+}
