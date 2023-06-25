3db9862be1428061f67433c65c85f520df359653
bananabread08 <rmkfermin@gmail.com>
2023-06-25 10:01:25 +0800

    add bg and remove unneccessary layouts

--- src/app/classes/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function layout({
-  children, // will be a page or nested layout
-}: {
-  children: React.ReactNode;
-}) {
-  return <section>{children}</section>;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { Navbar } from '@/components/Navbar';
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Inter, Manrope } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
+const manrope = Manrope({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'FEngage Calc',
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + ' text-clamp bg-slate-500'}>
+      <body
+        className={
+          manrope.className +
+          ' text-clamp min-h-screen bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900'
+        }
+      >
         <Navbar />
         <main>{children}</main>
       </body>
